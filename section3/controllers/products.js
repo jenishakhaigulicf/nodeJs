7b4a9a3b8a997a3fd4fd3af0504cfce68847d0c0
jenishakhaigulicf <jenishakhaiguli616@gmail.com>
2024-08-16 14:24:40 +0545

    Add product model

--- section3/controllers/products.js
+++ section3/controllers/products.js
@@ -1,36 +1,40 @@
-const products = [];
-
+const Product = require("../models/product");
 exports.getAddProduct = (req, res) => {
-    // NOTE: using html here itself
-    // res.send(
-    //   "<form action='/admin/product' type='submit' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
-    // );
-  
-    // NOTE: adding html in diff file
-    // res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  
-    // NOTE: using EJS
-    res.render("add-product", { pageTitle: "Add Product",  path:"/admin/add-product" });
-  }
+  // NOTE: using html here itself
+  // res.send(
+  //   "<form action='/admin/product' type='submit' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  // );
 
-  exports.postAddProduct = (req, res) => {
-    console.log("req.body", req.body);
-    products.push({ title: req.body.title });
-    res.redirect("/");
-  }
+  // NOTE: adding html in diff file
+  // res.sendFile(path.join(rootDir, "views", "add-product.html"));
 
-  exports.getProducts = (req, res) => {
-    // res.send("<h1>Hello from Express!</h1>");
-  
-    // NOTE: this won't work so use below solution
-    // res.sendFile('./views/shop.html')
-  
-    // __dirname : absolute path to our current file
-    // since shop is inside route we need to go out one level to reach to shop.html
-    // NOTE: this is static UI
-    // res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'))
-  
-    // NOTE: dynamic UI (pug)
-    res.render('shop', {prods: products, pageTitle: 'My Shop', path:"/"})
-  
-  }
+  // NOTE: using EJS
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+  });
+};
+
+exports.postAddProduct = (req, res) => {
+  console.log("req.body", req.body);
+  // products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res) => {
+  // res.send("<h1>Hello from Express!</h1>");
+
+  // NOTE: this won't work so use below solution
+  // res.sendFile('./views/shop.html')
+
+  // __dirname : absolute path to our current file
+  // since shop is inside route we need to go out one level to reach to shop.html
+  // NOTE: this is static UI
+  // res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'))
+
+  // NOTE: dynamic UI (pug/ejs)
+  const products = Product.fetchAll();
+  res.render("shop", { prods: products, pageTitle: "My Shop", path: "/" });
+};
