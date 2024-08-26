54dd56638c21c166e28d0056304a8dd1c84b2c96
Jenisha <jenisha@192.168.123.4>
2024-08-26 17:38:27 +0545

    feat: additional setup for learning session and cookies

--- 04-adding-a-cart-model/util/database.js
+++ 04-adding-a-cart-model/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
 // NOTE: Connection to database
-const sequelize = new Sequelize('node-complete', 'root', 'password12345678', {dialect: 'mysql', host: 'localhost'})
+const sequelize = new Sequelize('node-complete', 'root', 'root@123', {dialect: 'mysql', host: 'localhost'})
 
 module.exports = sequelize
