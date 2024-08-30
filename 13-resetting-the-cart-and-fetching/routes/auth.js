dfe33764c86e98854f0a4f21014231ab09521717
Jenisha <jenisha@192.168.1.6>
2024-08-30 08:37:48 +0545

    feat: setup for authentication

--- 13-resetting-the-cart-and-fetching/routes/auth.js
+++ 13-resetting-the-cart-and-fetching/routes/auth.js
@@ -1,12 +1,18 @@
 // step 1
 const express = require('express');
 
-const authController = require('../controllers/auth')
+const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin)
+router.get('/login', authController.getLogin);
 
-router.post('/login', authController.postLogin)
+router.get('/signup', authController.getSignup);
 
-module.exports = router
+router.post('/login', authController.postLogin);
+
+router.post('/signup', authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+module.exports = router;
