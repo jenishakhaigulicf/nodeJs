ddeb836e506ff3e1f998df6eef2c9df24c6c5879
Jenisha <jenisha@192.168.1.6>
2024-08-30 08:35:08 +0545

    feat: using session to check if user is logged in or not

--- 13-resetting-the-cart-and-fetching/app.js
+++ 13-resetting-the-cart-and-fetching/app.js
@@ -1,47 +1,51 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
-const errorController = require('./controllers/error');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/order-item');
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 // step 3
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({ secret: "my secret", resave: "false", saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findByPk(1)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err));
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 // step 4
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 User.hasOne(Cart);
 Cart.belongsTo(User);
@@ -54,23 +58,23 @@ Order.belongsToMany(Product, { through: OrderItem });
 sequelize
   // .sync({ force: true })
   .sync()
-  .then(result => {
+  .then((result) => {
     return User.findByPk(1);
     // console.log(result);
   })
-  .then(user => {
+  .then((user) => {
     if (!user) {
-      return User.create({ name: 'Max', email: 'test@test.com' });
+      return User.create({ name: "Max", email: "test@test.com" });
     }
     return user;
   })
-  .then(user => {
+  .then((user) => {
     // console.log(user);
     return user.createCart();
   })
-  .then(cart => {
+  .then((cart) => {
     app.listen(3000);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(err);
   });
--- 13-resetting-the-cart-and-fetching/controllers/auth.js
+++ 13-resetting-the-cart-and-fetching/controllers/auth.js
@@ -1,14 +1,15 @@
 // step 2
 exports.getLogin = (req, res, next) => {
-    const isAuthenticated = (req.get('Cookie').split("=")[1])
+    // const isAuthenticated = (req.get('Cookie').split("=")[1])
     res.render('auth/login',{
          path: '/login',
          pageTitle: "Login",
-         isAuthenticated
+         isAuthenticated : req.session.isLoggedIn
     })
 }
 
 exports.postLogin = (req, res) => {
-    res.setHeader('Set-Cookie', 'isLoggedIn=true')
+    // res.setHeader('Set-Cookie', 'isLoggedIn=true')
+    req.session.isLoggedIn = true 
     res.redirect('/')
 }
